561aaf1e1958b523eed33655bc30bd04df1bc208
Lsapee <dlworhks94@gmail.com>
2022-04-06 20:09:12 +0900

    compile test

--- Flutter_start/dart/test1.dart
+++ Flutter_start/dart/test1.dart
@@ -1,3 +1,14 @@
 void main() {
   print("hello");
 }
+// dart compile -o {파일명}.js {dart파일명}.dart -> js 파일로 만들기
+// dart compile -> dart 컴파일 가능한 목록이 나옴
+/*
+  compile : cpu가 이해할 수 있는 기계어로 변환 해주는 작업
+  인터프리터 -> 컴파일 없이 OS에 맞게 실행됨
+  JIT = 
+    1. 중간언어로 컴파일 VM이 존재함
+    2. 인터프리터 실행
+    3. 
+
+ */
